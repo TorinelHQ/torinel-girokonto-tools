69930b1dced579169114ad9e2bdb8dbef62efbf0
torinel-de <mutwaza.moseri@torinel.de>
2026-05-09 10:58:27 +0200

    Create index.js

---
+++ src/iban/index.js
@@ -0,0 +1,4 @@
+export { normalizeGermanIban } from "./normalizeGermanIban.js";
+export { formatGermanIban } from "./formatGermanIban.js";
+export { extractGermanBlz } from "./extractGermanBlz.js";
+export { isGermanIbanShape } from "./isGermanIbanShape.js";
